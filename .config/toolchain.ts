fb04672da327e473047f58fa0c90bd238ff7e21d
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2023-08-21 22:51:17 -0700

    wip moon

--- .config/toolchain.ts
+++ .config/toolchain.ts
@@ -1,6 +1,12 @@
-import { gitignore } from "../sources/features/gitignore/gitignore.js";
+import { gitignore } from "../packages/features/gitignore/gitignore.js";
+import { moon } from "../packages/features/moon/moon.js";
 
 export default {
   engine: "bun@latest",
-  features: [gitignore()],
-} satisfies import("../sources/configTypes").RepoConfig;
+  features: [
+    gitignore(),
+    moon({
+      workspace: { projects: ["packages/features/*", "packages/platform/*"] },
+    }),
+  ],
+} satisfies import("../packages/platform/core/configTypes.js").RepoConfig;
