46f34e41aaeb5d8433756c3d631d2947b91320d8
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2023-10-16 23:41:23 -0700

    wip

--- packages/platform/update-specifiers/main.ts
+++ packages/platform/update-specifiers/main.ts
@@ -3,12 +3,14 @@ import {
   type SourceFile,
   getCompilerOptionsFromTsConfig,
   ts,
+  type FileSystemHost,
 } from "ts-morph";
 import {
   TransactionalFileSystem,
   RealFileSystemHost,
   InMemoryFileSystemHost,
   TsConfigResolver,
+  type RuntimeDirEntry,
 } from "@ts-morph/common";
 import path from "node:path";
 
@@ -47,6 +49,93 @@ type PrivateTsConfigResolver = Omit<
   parseJsonConfigFileContent: () => ts.ParsedCommandLine;
 };
 
+class OverlayMemoryOnReadOnlyRealFileSystem
+  extends InMemoryFileSystemHost
+  implements FileSystemHost
+{
+  constructor(private readonly realFs: FileSystemHost) {
+    super();
+  }
+
+  override async directoryExists(dirPath: string): Promise<boolean> {
+    const memResult = await super.directoryExists(dirPath);
+    if (memResult) return true;
+    return this.realFs.directoryExists(dirPath);
+  }
+
+  override directoryExistsSync(dirPath: string): boolean {
+    const memResult = super.directoryExistsSync(dirPath);
+    if (memResult) return true;
+    return this.realFs.directoryExistsSync(dirPath);
+  }
+
+  override async fileExists(filePath: string): Promise<boolean> {
+    const memResult = await super.fileExists(filePath);
+    if (memResult) return true;
+    return this.realFs.fileExists(filePath);
+  }
+
+  override fileExistsSync(filePath: string): boolean {
+    const memResult = super.fileExistsSync(filePath);
+    if (memResult) return true;
+    return this.realFs.fileExistsSync(filePath);
+  }
+
+  override getCurrentDirectory(): string {
+    return this.realFs.getCurrentDirectory();
+  }
+
+  override async readFile(
+    filePath: string,
+    encoding?: string,
+  ): Promise<string> {
+    const memResult = await super
+      .readFile(filePath, encoding)
+      .catch(() => undefined);
+    if (memResult !== undefined) return memResult;
+    return this.realFs.readFile(filePath, encoding);
+  }
+
+  override readFileSync(
+    filePath: string,
+    encoding?: string | undefined,
+  ): string {
+    try {
+      const memResult = super.readFileSync(filePath, encoding);
+      if (memResult) return memResult;
+    } catch {
+      // ignore
+    }
+    return this.realFs.readFileSync(filePath, encoding);
+  }
+
+  override realpathSync(path: string): string {
+    try {
+      const memResult = super.realpathSync(path);
+      if (memResult) return memResult;
+    } catch {
+      // ignore
+    }
+    return this.realFs.realpathSync(path);
+  }
+
+  override readDirSync(dirPath: string): RuntimeDirEntry[] {
+    try {
+      const memResult = super.readDirSync(dirPath);
+      if (memResult.length) return memResult;
+    } catch {
+      // ignore
+    }
+    return this.realFs.readDirSync(dirPath);
+  }
+
+  override async glob(patterns: readonly string[]): Promise<string[]> {
+    const memResult = await super.glob(patterns);
+    if (memResult.length) return memResult;
+    return this.realFs.glob(patterns);
+  }
+}
+
 const renameSpecifiers = async ({
   tsConfigFilePath,
   fsExtensionMapping,
@@ -62,6 +151,7 @@ const renameSpecifiers = async ({
     skipLoadingLibFiles: true,
     libFolderPath: undefined,
   });
+  // const memoryFs = new OverlayMemoryOnReadOnlyRealFileSystem(fsHost);
   const memoryFs = new InMemoryFileSystemHost();
 
   const standardizedTsConfigPath =
@@ -134,8 +224,8 @@ const renameSpecifiers = async ({
         project: tsConfigPath,
       },
       skipAddingFilesFromTsConfig: true,
-      skipFileDependencyResolution: true,
-      skipLoadingLibFiles: true,
+      // skipFileDependencyResolution: true,
+      // skipLoadingLibFiles: true,
     });
 
     tsConfigPathToRemappedProject.set(tsConfigPath, remappedProject);
@@ -160,10 +250,10 @@ const renameSpecifiers = async ({
           remappedProject,
         });
 
-        const preemitDiag =
-          remappedProject.formatDiagnosticsWithColorAndContext(
-            remappedProject.getPreEmitDiagnostics(),
-          );
+        // const preemitDiag =
+        //   remappedProject.formatDiagnosticsWithColorAndContext(
+        //     remappedProject.getPreEmitDiagnostics(),
+        //   );
 
         // console.log(preemitDiag);
 
@@ -188,31 +278,25 @@ const renameSpecifiers = async ({
           //   ),
           // );
 
-          // return outputFiles.map(async (emittedFile) => {
-          //   const filePath = emittedFile.getFilePath();
-          //   const fileDir = path.dirname(filePath);
-          //   await fsHost.mkdir(fileDir);
-          //   await Promise.all([
-          //     fsHost.writeFile(filePath, emittedFile.getText()),
-          //     !sourceTextEmitted &&
-          //       fsHost
-          //         .writeFile(path.join(fileDir, baseName), sourceText)
-          //         .then(() => {
-          //           sourceTextEmitted = true;
-          //         }),
-          //   ]);
-          // });
+          return outputFiles.map(async (emittedFile) => {
+            const filePath = emittedFile.getFilePath();
+            const fileDir = path.dirname(filePath);
+            await fsHost.mkdir(fileDir);
+            await Promise.all([
+              fsHost.writeFile(filePath, emittedFile.getText()),
+              !sourceTextEmitted &&
+                fsHost
+                  .writeFile(path.join(fileDir, baseName), sourceText)
+                  .then(() => {
+                    sourceTextEmitted = true;
+                  }),
+            ]);
+          });
         });
       },
     ),
   );
 };
-
-renameSpecifiers({
-  tsConfigFilePath: "./tsconfig.json",
-  fsExtensionMapping: { ".ts": ".mts" },
-  emittedExtensionMapping: { ".ts": ".mjs" },
-});
 
 function updateChangeSets({
   sourceFiles,
@@ -390,3 +474,9 @@ function replaceExtensionInLiteral(
     )
   );
 }
+
+renameSpecifiers({
+  tsConfigFilePath: "./tsconfig.json",
+  fsExtensionMapping: { ".ts": ".cts" },
+  emittedExtensionMapping: { ".ts": ".cjs" },
+});
