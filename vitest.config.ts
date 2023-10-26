39ee190bfa422a8d8180c3df2c0809100e702b6d
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2023-10-25 20:21:47 -0700

    chore: upgrade packages

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,43 +1,44 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import { swc, defineRollupSwcOption } from "rollup-plugin-swc3";
+
+const promock = {
+  ...swc(
+    defineRollupSwcOption({
+      swcrc: false,
+      configFile: false,
+      tsconfig: false,
+      minify: false,
+      sourceMaps: true,
+      module: {
+        type: "nodenext",
+      },
+      jsc: {
+        target: "esnext",
+        preserveAllComments: true,
+        keepClassNames: true,
+        experimental: {
+          plugins: [
+            [
+              "swc-mockify",
+              {
+                basePath: __dirname,
+                importFrom: "swc-mockify/src/mockify.ts",
+              },
+            ],
+          ],
+          disableBuiltinTransformsForInternalTesting: true,
+        },
+      },
+    }),
+  ),
+  name: "promock",
+} as PluginOption;
 
 export default defineConfig({
   test: {
     // ...
   },
-  plugins: [
-    {
-      ...swc(
-        defineRollupSwcOption({
-          swcrc: false,
-          configFile: false,
-          tsconfig: false,
-          minify: false,
-          sourceMaps: true,
-          module: {
-            type: "nodenext",
-          },
-          jsc: {
-            target: "esnext",
-            preserveAllComments: true,
-            keepClassNames: true,
-            experimental: {
-              plugins: [
-                [
-                  "swc-mockify",
-                  {
-                    basePath: __dirname,
-                    importFrom: "swc-mockify/src/mockify.ts",
-                  },
-                ],
-              ],
-              disableBuiltinTransformsForInternalTesting: true,
-            },
-          },
-        }),
-      ),
-      name: "promock",
-    },
-  ],
+  plugins: [promock],
 });
