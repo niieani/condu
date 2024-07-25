a03b63ecd905b4b0df5f1f3bf570435c70bcd905
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2024-07-24 22:49:49 -0700

    feat: add sync version of findUp

--- packages/platform/core/utils/findUp.ts
+++ packages/platform/core/utils/findUp.ts
@@ -1,10 +1,24 @@
 import * as path from "node:path";
 import * as fs from "node:fs";
 
+export type SyncFileTestFunction = (file: fs.Dirent) => boolean;
+
 export type FileTestFunction =
-  | ((file: fs.Dirent) => boolean)
+  | SyncFileTestFunction
   | ((file: fs.Dirent) => Promise<boolean>);
 export type PossibleNameTypes = string | string[] | RegExp | FileTestFunction;
+export type SyncPossibleNameTypes =
+  | string
+  | string[]
+  | RegExp
+  | SyncFileTestFunction;
+
+export interface Options {
+  cwd?: string;
+  type?: "file" | "directory" | "any";
+  allowSymlinks?: boolean;
+  stopAt?: string;
+}
 
 async function* findUpGenerator(
   test: FileTestFunction,
@@ -36,11 +50,31 @@ async function* findUpGenerator(
   yield* findUpGenerator(test, parentDir);
 }
 
-export interface Options {
-  cwd?: string;
-  type?: "file" | "directory" | "any";
-  allowSymlinks?: boolean;
-  stopAt?: string;
+// sync version:
+function* findUpGeneratorSync(
+  test: SyncFileTestFunction,
+  cwd: string,
+  stopAt?: string,
+): Generator<string, undefined, undefined> {
+  try {
+    const files = fs.readdirSync(cwd, { withFileTypes: true });
+    let match = false;
+    for (const file of files) {
+      const testResult = test(file);
+      if (testResult) {
+        yield path.join(cwd, file.name);
+        match = true;
+      }
+    }
+    if (match) return;
+  } catch (err) {
+    // Ignore error and continue walking up
+  }
+  const parentDir = path.dirname(cwd);
+  if (parentDir === cwd || parentDir === stopAt) {
+    return;
+  }
+  yield* findUpGeneratorSync(test, parentDir);
 }
 
 export function getFindUpGenerator(
@@ -76,12 +110,55 @@ export function getFindUpGenerator(
   return findUpGenerator(test, cwd, stopAt);
 }
 
+// sync version:
+export function getFindUpGeneratorSync(
+  name: SyncPossibleNameTypes,
+  {
+    cwd = process.cwd(),
+    type = "file",
+    allowSymlinks = true,
+    stopAt,
+  }: Options = {},
+): Generator<string, undefined, undefined> {
+  const testType =
+    type === "any"
+      ? () => true
+      : type === "file"
+        ? (file: fs.Dirent) => file.isFile()
+        : (file: fs.Dirent) => file.isDirectory();
+  const testSymlink = allowSymlinks
+    ? () => true
+    : (file: fs.Dirent) => !file.isSymbolicLink();
+  const test =
+    typeof name === "function"
+      ? name
+      : typeof name === "string"
+        ? (file: fs.Dirent) =>
+            file.name === name && testType(file) && testSymlink(file)
+        : Array.isArray(name)
+          ? (file: fs.Dirent) =>
+              name.includes(file.name) && testType(file) && testSymlink(file)
+          : (file: fs.Dirent) =>
+              name.test(file.name) && testType(file) && testSymlink(file);
+  return findUpGeneratorSync(test, cwd, stopAt);
+}
+
 export async function findUp(
   name: PossibleNameTypes,
   options: Options = {},
 ): Promise<string | undefined> {
   const generator = getFindUpGenerator(name, options);
   const result = await generator.next();
+  return result.value;
+}
+
+// sync version:
+export function findUpSync(
+  name: SyncPossibleNameTypes,
+  options: Options = {},
+): string | undefined {
+  const generator = getFindUpGeneratorSync(name, options);
+  const result = generator.next();
   return result.value;
 }
 
@@ -92,6 +169,22 @@ export async function findUpMultiple(
   const generator = getFindUpGenerator(name, options);
   const files: string[] = [];
   for await (const file of generator) {
+    files.push(file);
+    if (options.limit && files.length >= options.limit) {
+      break;
+    }
+  }
+  return files;
+}
+
+// sync version:
+export function findUpMultipleSync(
+  name: SyncPossibleNameTypes,
+  options: Options & { limit?: number } = {},
+): string[] {
+  const generator = getFindUpGeneratorSync(name, options);
+  const files: string[] = [];
+  for (const file of generator) {
     files.push(file);
     if (options.limit && files.length >= options.limit) {
       break;
